57fe23e0b8ea18b30a9f0dc57b23191c13dd4c35
Pradeep419 <37213794+Pradeep419@users.noreply.github.com>
2019-11-14 11:58:14 +0530

    Test
    Test

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SampleProjectonCI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("est");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Close();
+        }
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+    }
+}
